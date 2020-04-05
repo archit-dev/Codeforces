c0fb7b7e0b0e238c8d6717adda60a32f8bdf2709
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2020-04-05 20:26:07 +0530

    Create 431A - Black Square.cpp

---
+++ 431A - Black Square.cpp
@@ -0,0 +1,33 @@
+//By archit-dev, contest: Codeforces Round #247 (Div. 2), problem: (A) Black Square, Accepted, #, Copy
+
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+using namespace std;
+  
+int search(int arr[],int key/*,string s*/){
+	switch(key){
+		case '1':return arr[0];
+		case '2': return arr[1];
+		case '3': return arr[2];
+		case '4': return arr [3];
+		}
+	return -1;
+}
+ 
+int main(){
+	int x;
+	int arr[4];
+	for(int i=0;i<4;i++){
+		cin>>x;
+		arr[i]=x;
+	}
+	string s;
+	cin>>s;
+	x=0;
+	for(int i=0;i<s.size();i++){
+		x+=search(arr,s[i]);
+	}
+	cout<<x<<endl;
+	return 0;
+}
