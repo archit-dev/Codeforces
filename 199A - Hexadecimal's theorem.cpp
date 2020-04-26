2c233258cd79db42cfeb6ca979b6144eac0a7c0b
Archit Dev <62344448+archit-dev@users.noreply.github.com>
2020-04-26 11:52:16 +0530

    Create 199A - Hexadecimal's theorem.cpp

---
+++ 199A - Hexadecimal's theorem.cpp
@@ -0,0 +1,24 @@
+//By archit-dev, contest: Codeforces Round #125 (Div. 2), problem: (A) Hexadecimal's theorem, Accepted, #, Copy
+
+#include <bits/stdc++.h>
+#include <string>
+#include <algorithm>
+#include <cmath>
+#define ll long long int
+#define ull unsigned long long int 
+#define pb push_back
+ 
+ 
+using namespace std;
+ 
+int main(){
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+		int n;
+		cin>>n;
+		cout<<"0 0 "<<n<<"\n";
+    return 0;
+ 
+} 
